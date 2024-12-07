b509d8882bd0aab9e876d1cef76286a8bafcc8c9
Messie Osango <messieosango33@gmail.com>
2024-12-07 17:59:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,238 +1,218 @@
-document.getElementById('agreeCheckbox').addEventListener('change', function() {
-	document.getElementById('submitButton').disabled = !this.checked;
-});
-let Commands = [{
-	'commands': []
-}, {
-	'handleEvent': []
-}];
-
-function measurePing() {
-	var xhr = new XMLHttpRequest();
-	var startTime, endTime;
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4) {
-			endTime = Date.now();
-			var pingTime = endTime - startTime;
-			document.getElementById("ping").textContent = pingTime + " ms";
-		}
-	};
-	xhr.open("GET", location.href + "?t=" + new Date().getTime());
-	startTime = Date.now();
-	xhr.send();
-}
-setInterval(measurePing, 1000);
-
-function updateTime() {
-	const now = new Date();
-	const options = {
-		timeZone: 'Asia/Manila',
-		hour12: true,
-		hour: 'numeric',
-		minute: 'numeric',
-		second: 'numeric'
-	};
-	const formattedTime = now.toLocaleString('en-US', options);
-	document.getElementById('time').textContent = formattedTime;
-}
-updateTime();
-setInterval(updateTime, 1000);
-async function State() {
-	const jsonInput = document.getElementById('json-data');
-	const button = document.getElementById('submitButton');
-	if (!Commands[0].commands.length) {
-		return showResult('Please provide at least one valid command for execution.');
-	}
-	try {
-		button.style.display = 'none';
-		const State = JSON.parse(jsonInput.value);
-		if (State && typeof State === 'object') {
-			const response = await fetch('/login', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					state: State,
-					commands: Commands,
-					prefix: document.getElementById('inputOfPrefix').value,
-					admin: document.getElementById('inputOfAdmin').value,
-				}),
-			});
-			const data = await response.json();
-			if (data.success) {
-				jsonInput.value = '';
-				showResult(data.message);
-			} else {
-				jsonInput.value = '';
-				showResult(data.message);
-			}
-		} else {
-			jsonInput.value = '';
-			showResult('Invalid JSON data. Please check your input.');
-		}
-	} catch (parseError) {
-		jsonInput.value = '';
-		console.error('Error parsing JSON:', parseError);
-		showResult('Error parsing JSON. Please check your input.');
-	} finally {
-		setTimeout(() => {
-			button.style.display = 'block';
-		}, 4000);
-	}
+body {
+	background-color: white;
+	text-align: center;
+	font-family: sans-serif;
 }
 
-function showResult(message) {
-	const resultContainer = document.getElementById('result');
-	resultContainer.innerHTML = `<h5>${message}</h5>`;
-	resultContainer.style.display = 'block';
+* {
+margin: 0;
+padding: 0;
+position: flex;
 }
-async function commandList() {
-	try {
-		const [listOfCommands, listOfCommandsEvent] = [document.getElementById('listOfCommands'), document.getElementById('listOfCommandsEvent')];
-		const response = await fetch('/commands');
-		const {
-			commands,
-			handleEvent,
-			aliases
-		} = await response.json();
-		[commands, handleEvent].forEach((command, i) => {
-			command.forEach((command, index) => {
-				const container = createCommand(i === 0 ? listOfCommands : listOfCommandsEvent, index + 1, command, i === 0 ? 'commands' : 'handleEvent', aliases[index] || []);
-				i === 0 ? listOfCommands.appendChild(container) : listOfCommandsEvent.appendChild(container);
-			});
-		});
-	} catch (error) {
-		console.log(error);
-	}
+.bodydark {
+	background-color: black;
 }
 
-function createCommand(element, order, command, type, aliases) {
-	const container = document.createElement('div');
-	container.classList.add('form-check', 'form-switch');
-	container.onclick = toggleCheckbox;
-	const checkbox = document.createElement('input');
-	checkbox.classList.add('form-check-input', type === 'handleEvent' ? 'handleEvent' : 'commands');
-	checkbox.type = 'checkbox';
-	checkbox.role = 'switch';
-	checkbox.id = `flexSwitchCheck_${order}`;
-	const label = document.createElement('label');
-	label.classList.add('form-check-label', type === 'handleEvent' ? 'handleEvent' : 'commands');
-	label.for = `flexSwitchCheck_${order}`;
-	label.textContent = `${order}. ${command}`;
-	container.appendChild(checkbox);
-	container.appendChild(label);
-	/*
-	if (aliases.length > 0 && type !== 'handleEvent') {
-		const aliasText = document.createElement('span');
-		aliasText.classList.add('aliases');
-		aliasText.textContent = ` (${aliases.join(', ')})`;
-		label.appendChild(aliasText);
-	}
-	*/
-	return container;
+h2 {
+	color: black;
+	margin-bottom: 9px;
+	margin-top: 0;
+}
+.h2dark {
+	color: white;
 }
 
-function toggleCheckbox() {
-	const box = [{
-		input: '.form-check-input.commands',
-		label: '.form-check-label.commands',
-		array: Commands[0].commands
-	}, {
-		input: '.form-check-input.handleEvent',
-		label: '.form-check-label.handleEvent',
-		array: Commands[1].handleEvent
-	}];
-	box.forEach(({
-		input,
-		label,
-		array
-	}) => {
-		const checkbox = this.querySelector(input);
-		const labelText = this.querySelector(label);
-		if (checkbox) {
-			checkbox.checked = !checkbox.checked;
-			if (checkbox.checked) {
-				labelText.classList.add('disable');
-				const command = labelText.textContent.replace(/^\d+\.\s/, '').split(" ")[0];
-				array.push(command);
-			} else {
-				labelText.classList.remove('disable');
-				const command = labelText.textContent.replace(/^\d+\.\s/, '').split(" ")[0];
-				const removeCommand = array.indexOf(command);
-				if (removeCommand !== -1) {
-					array.splice(removeCommand, 1);
-				}
-			}
-		}
-	});
+.top {
+	text-align: right;
+	justify-content: center;
 }
 
-function selectAllCommands() {
-	const box = [{
-		input: '.form-check-input.commands',
-		array: Commands[0].commands
-	}];
-	box.forEach(({
-		input,
-		array
-	}) => {
-		const checkboxes = document.querySelectorAll(input);
-		const allChecked = Array.from(checkboxes).every(checkbox => checkbox.checked);
-		checkboxes.forEach((checkbox) => {
-			if (allChecked) {
-				checkbox.checked = false;
-				const labelText = checkbox.nextElementSibling;
-				labelText.classList.remove('disable');
-				const command = labelText.textContent.replace(/^\d+\.\s/, '').split(" ")[0];
-				const removeCommand = array.indexOf(command);
-				if (removeCommand !== -1) {
-					array.splice(removeCommand, 1);
-				}
-			} else {
-				checkbox.checked = true;
-				const labelText = checkbox.nextElementSibling;
-				labelText.classList.add('disable');
-				const command = labelText.textContent.replace(/^\d+\.\s/, '').split(" ")[0];
-				if (!array.includes(command)) {
-					array.push(command);
-				}
-			}
-		});
-	});
+.settings {
+	text-align: right;
+	margin-right:10px;
+	border: none;
+	margin-bottom: 0;
+	font-size: 25px;
 }
 
-function selectAllEvents() {
-	const box = [{
-		input: '.form-check-input.handleEvent',
-		array: Commands[1].handleEvent
-	}];
-	box.forEach(({
-		input,
-		array
-	}) => {
-		const checkboxes = document.querySelectorAll(input);
-		const allChecked = Array.from(checkboxes).every(checkbox => checkbox.checked);
-		checkboxes.forEach((checkbox) => {
-			if (allChecked) {
-				checkbox.checked = false;
-				const labelText = checkbox.nextElementSibling;
-				labelText.classList.remove('disable');
-				const event = labelText.textContent.replace(/^\d+\.\s/, '').split(" ")[0];
-				const removeEvent = array.indexOf(event);
-				if (removeEvent !== -1) {
-					array.splice(removeEvent, 1);
-				}
-			} else {
-				checkbox.checked = true;
-				const labelText = checkbox.nextElementSibling;
-				labelText.classList.add('disable');
-				const event = labelText.textContent.replace(/^\d+\.\s/, '').split(" ")[0];
-				if (!array.includes(event)) {
-					array.push(event);
-				}
-			}
-		});
-	});
+.darksettings {
+color: white;
 }
-commandList();
+
+.score {
+	font-weight: bold;
+	font-size: 16px;
+	margin-left: 20px;
+	text-align:left;
+	margin-right: 120px;
+	color: black;
+}
+
+.darkscore {
+	color: white;
+}
+.highscore {
+	font-weight: bold;
+	text-align: right;
+	margin-right: 30px;
+	font-size: 16px;
+	color: black;
+}
+
+.darkHighScore {
+	color: white;
+}
+canvas {
+	border: 3px solid black;
+	margin:5px;
+	border-radius: 5px;
+}
+
+.canvasdark {
+	border: 3px solid white;	
+}
+.p {
+	padding-left: 15px;
+	padding-right: 15px;
+	padding-top: 10px;
+	padding-bottom: 10px;
+	margin: 6px;
+	font-weight: bold;
+	font-size: 28px;
+	border: 4px solid black;
+	background-color: white;
+	border-radius: 8px;
+	text-align: center;
+	color: black;
+}
+
+.pdark {
+color: white;
+border: 4px solid white;
+background-color: black;
+}
+.menu {
+text-align: center;
+}
+
+.restart {
+	font-size: 25px;
+	font-weight: bold;
+}
+
+.restartdark {
+	background-color: black;
+	color: white;
+}
+
+.pause {
+	font-size: 19px;
+	font-weight: bold;
+	padding-left: 30px;
+	padding-right: 30px;
+	
+}
+
+.pausedark {
+	background-color: black;
+	color: white;
+}
+.play {
+	font-weight: bold;
+	margin-left: 7px;
+	font-size: 20px;
+	margin-bottom: 10px;
+}
+.darkplay {
+	background-color: black;
+	color: white;
+}
+.showsett {
+	width: 300px;
+	height: 200px;
+	border: 1px solid gray;
+	position:absolute;
+	margin: 0;
+	background-color: white;
+	text-align: left;
+	visibility: hidden;
+	border-radius: 10px;
+	top: 21%;
+	right: 7%;
+}
+
+.closesettings {
+font-weight: bold;
+background-color: white;
+font-size: 30px;
+margin-left: 13px;
+}
+
+.level {
+margin-left: 8px;
+font-size: 14px;
+}
+.open-settings{
+	visibility: visible;
+}
+
+.set {
+text-align: right;
+margin-top: 5px;
+}
+
+.darkmode {
+	margin-left: 10px;
+	border-radius: 50px;
+	font-size: 15px;
+	color: black;
+}
+
+.help {
+color: black;
+font-size: 25px;
+margin-right: 280px;
+border: 1px solid black;
+padding-left: 9px;
+padding-right: 9px;
+border-radius: 50px;
+}
+.darkhelp {
+	color: white;
+}
+
+.showhelp {
+width: 350px;
+height: 585px;
+border: 2px solid gray;
+position:absolute;
+margin: 0;
+background-color: white;
+top: 1%;
+left: 1%;
+text-align: left;
+visibility: hidden;
+
+border-radius: 10px;
+}
+
+.open-help {
+visibility: visible;
+}
+
+.closehelp {
+font-weight: bold;
+background-color: white;
+font-size: 30px;
+margin-left: 13px;
+border: none;
+}
+.ht {
+margin-left: 12px;
+}
+.helpsection {
+text-align: center;
+font-weight: bold;
+border: 1px solid black;
+border-radius: 5px;
+}
